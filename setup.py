6b7f282983986d07c0df871fecad8033f6575b3f
xbb <xbb66kw@gmail.com>
2025-03-10 14:02:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ohot",
+    name="ohos",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "scikit-learn",
         "hyperopt"
+        "treeple"
     ],
 )
